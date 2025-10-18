7240c286547b28fc18fee4e80dda63a055a6cf5a
klasok <klasokkl@gmail.com>
2025-10-18 09:15:11 +0000

    add beetroot

---
+++ beetroot_dz/lesson1/homework/task2.py
@@ -0,0 +1,15 @@
+name = input('whath your name? ')
+day = input('how many days do you study? ')
+print('Good day ' + name + '! is a perfect ' + day + ' to learn some python.')
+
+first_name = input('What your first name? ')
+last_name = input('What your last name? ')
+print('Сongratulations '+ first_name + ' ' + last_name)
+
+a = input('input a ')
+b = input('input b ')
+
+print('Addition ', a+b)
+print('Subtraction ', a-b)
+print('Division ', a/b)
+print('Multiplication', a*b)
